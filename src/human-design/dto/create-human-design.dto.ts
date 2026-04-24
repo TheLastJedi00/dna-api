26cab7525807013df35db5aa4becf74619183757
TheLastJedi00 <jediaelborges23@gmail.com>
2026-04-24 19:09:42 -0300

    dto e model criados

--- src/human-design/dto/create-human-design.dto.ts
+++ src/human-design/dto/create-human-design.dto.ts
@@ -1,1 +1,85 @@
-export class CreateHumanDesignDto {}
+import { IsArray, IsNumber, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CentrosEnergeticosDto {
+  @IsString({ each: true })
+  definidos!: string[];
+  @IsString({ each: true })
+  indefinidos!: string[];
+  @IsString({ each: true })
+  abertos!: string[];
+}
+
+export class CanalDto {
+  @IsString()
+  id!: string;
+  @IsString()
+  nome!: string;
+}
+
+export class PontosAtivacaoDto {
+  @IsNumber()
+  sol!: number;
+  @IsNumber()
+  terra!: number;
+  @IsNumber()
+  lua!: number;
+}
+
+export class AtivacoesDto {
+  @ValidateNested()
+  @Type(() => PontosAtivacaoDto)
+  personalidade!: PontosAtivacaoDto;
+  @ValidateNested()
+  @Type(() => PontosAtivacaoDto)
+  desenho!: PontosAtivacaoDto;
+}
+
+export class CruzEncarnacaoDto {
+  @IsString()
+  angulo!: string;
+  @IsString()
+  cruz!: string;
+  @IsString()
+  portoes!: string;
+  @IsString()
+  quarto_de_cruz!: string;
+}
+
+export class CreateHumanDesignDto {
+  @IsString()
+  tipo_aurico!: string;
+  @IsString()
+  aura!: string;
+  @IsString()
+  energia!: string;
+  @IsString()
+  palavra_chave!: string;
+  @IsString()
+  estrategia!: string;
+  @IsString()
+  assinatura!: string;
+  @IsString()
+  tema_do_nao_ser!: string;
+  @IsString()
+  autoridade!: string;
+  @IsString()
+  perfil!: string;
+
+  @ValidateNested()
+  @Type(() => CentrosEnergeticosDto)
+  centros_energeticos!: CentrosEnergeticosDto;
+
+  @ValidateNested({ each: true })
+  @Type(() => CanalDto)
+  @IsArray()
+  canais!: CanalDto[];
+
+  @ValidateNested()
+  @Type(() => AtivacoesDto)
+  ativacoes!: AtivacoesDto;
+
+  @ValidateNested()
+  @Type(() => CruzEncarnacaoDto)
+  encarnacao!: CruzEncarnacaoDto;
+}
--- src/human-design/entities/human-design.model.ts
+++ src/human-design/entities/human-design.model.ts
@@ -1,3 +1,28 @@
+import { randomUUID } from 'crypto';
+
 export class HumanDesign {
-    
+  id: string = randomUUID();
+  tipo_aurico!: string;
+  aura!: string;
+  energia!: string;
+  palavra_chave!: string;
+  estrategia!: string;
+  assinatura!: string;
+  tema_do_nao_ser!: string;
+  autoridade!: string;
+  perfil!: string;
+  centros_energeticos!: any;
+  canais!: any[];
+  ativacoes!: any;
+  encarnacao!: any;
+
+  constructor(partial?: Partial<HumanDesign>) {
+    if (partial) {
+      Object.assign(this, partial);
+    }
+  }
+
+  toPlainObject() {
+    return JSON.parse(JSON.stringify(this));
+  }
 }
