07fcf97a1fb3d920cc092348f289dae01a4c7f00
TheLastJedi00 <jediaelborges23@gmail.com>
2026-04-24 19:11:01 -0300

    fix imports do módulo human design

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HumanDesignController } from './human-design/human-design.controller';
-import { HumanDesignService } from './human-design/human-design.service';
-import { HumanDesignModule } from './human-design/human-design.module';
-import { HumanDesignModule } from './human-design/human-design.module';
 import { HumanDesignModule } from './human-design/human-design.module';
 
 @Module({
   imports: [HumanDesignModule],
-  controllers: [AppController, HumanDesignController],
-  providers: [AppService, HumanDesignService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
