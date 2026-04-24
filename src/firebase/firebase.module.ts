434be2cf7b3b3a4e81bcaf5c308c84bccf5808db
TheLastJedi00 <jediaelborges23@gmail.com>
2026-04-24 20:01:30 -0300

    feat: teste com variável de ambiente configurada

--- src/firebase/firebase.module.ts
+++ src/firebase/firebase.module.ts
@@ -1,6 +1,13 @@
-import * as admin from "firebase-admin";
+import * as admin from 'firebase-admin';
 
-var serviceAccount = require('../../dna_empresaria_6c142_firebase_adminsdk_fbsvc_1a29b09a39.json');
+require("dotenv").config();
+const serviceKeyBase64 = process.env.FIREBASE_SERVICE_ACCOUNT_JSON;
+if (!serviceKeyBase64) {
+  throw new Error('Chave de Conta de Serviço não encontrada');
+}
+const buffer = Buffer.from(serviceKeyBase64, 'base64')
+const serviceKeyString = buffer.toString('utf-8')
+const serviceAccount = JSON.parse(serviceKeyString);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
