d9bb421d3c2271a581f0adafc209cfa9058b0f7a
TheLastJedi00 <jediaelborges23@gmail.com>
2026-07-06 15:02:44 -0300

    refactor(pillars): PillarService base unifica create/findOne/findOneByUser/checkExistence/update/remove [T2.6]

--- src/astrology/astrology.service.ts
+++ src/astrology/astrology.service.ts
@@ -1,46 +1,21 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateAstrologyDto } from './dto/create-astrology.dto';
 import { UpdateAstrologyDto } from './dto/update-astrology.dto';
 import { AstrologyRepository } from './astrology.repository';
 import { Astrology } from './entities/astrology.entity';
+import { PillarService } from '../common/pillar.service';
 
 @Injectable()
-export class AstrologyService {
-  constructor(private readonly repository: AstrologyRepository) {}
-
-  async create(dto: CreateAstrologyDto) {
-    const obj = new Astrology(dto);
-    await this.repository.create(obj);
+export class AstrologyService extends PillarService<
+  Astrology,
+  CreateAstrologyDto,
+  UpdateAstrologyDto
+> {
+  constructor(repository: AstrologyRepository) {
+    super(repository, 'astrology');
   }
 
-  async findOne(id: string) {
-    const data = await this.repository.findById(id);
-    if (!data) {
-      throw new NotFoundException(`Doc with ID ${id} not found in astrology collection.`);
-    }
-    return data;
-  }
-
-  async findOneByUser(userId: string) {
-    const data = await this.repository.findByUserId(userId);
-    if (!data) {
-      throw new NotFoundException(`Doc with User ID ${userId} not found in astrology collection.`);
-    }
-    return data;
-  }
-
-  async checkExistence(userId: string){
-    const search = await this.repository.findByUserId(userId);
-    return !!search
-  }
-
-  async update(id: string, dto: UpdateAstrologyDto) {
-    await this.findOne(id);
-    return this.repository.update(id, dto);
-  }
-
-  async remove(id: string) {
-    await this.findOne(id);
-    return this.repository.delete(id);
+  protected instantiate(dto: CreateAstrologyDto): Astrology {
+    return new Astrology(dto);
   }
 }
--- src/human-design/human-design.service.ts
+++ src/human-design/human-design.service.ts
@@ -1,46 +1,21 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateHumanDesignDto } from './dto/create-human-design.dto';
 import { UpdateHumanDesignDto } from './dto/update-human-design.dto';
 import { HumanDesignRepository } from './human-design.repository';
 import { HumanDesign } from './entities/human-design.model';
+import { PillarService } from '../common/pillar.service';
 
 @Injectable()
-export class HumanDesignService {
-  constructor(private readonly repository: HumanDesignRepository) {}
-
-  async create(dto: CreateHumanDesignDto) {
-    const obj = new HumanDesign(dto);
-    await this.repository.create(obj);
+export class HumanDesignService extends PillarService<
+  HumanDesign,
+  CreateHumanDesignDto,
+  UpdateHumanDesignDto
+> {
+  constructor(repository: HumanDesignRepository) {
+    super(repository, 'human-design');
   }
 
-  async findOne(id: string) {
-    const data = await this.repository.findById(id);
-    if (!data) {
-      throw new NotFoundException(`Doc with ID ${id} not found in human-design collection.`);
-    }
-    return data;
-  }
-
-  async findOneByUser(userId: string) {
-    const data = await this.repository.findByUserId(userId);
-    if (!data) {
-      throw new NotFoundException(`Doc with User ID ${userId} not found in human-design collection.`);
-    }
-    return data;
-  }
-
-  async checkExistence(userId: string){
-    const search = await this.repository.findByUserId(userId);
-    return !!search
-  }
-
-  async update(id: string, dto: UpdateHumanDesignDto) {
-    await this.findOne(id);
-    return this.repository.update(id, dto);
-  }
-
-  async remove(id: string) {
-    await this.findOne(id);
-    return this.repository.delete(id);
+  protected instantiate(dto: CreateHumanDesignDto): HumanDesign {
+    return new HumanDesign(dto);
   }
 }
